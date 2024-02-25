6a0fd7d48a32bd1d5c4a5d4858913206a68544bb
mysticinferno <110055773+mysticinferno@users.noreply.github.com>
2024-02-25 14:14:27 +0530

    Add files via upload

---
+++ print_requests.py
@@ -0,0 +1,21 @@
+import pickle
+
+def storeData(obj, filename):
+    pickleFile = open(filename, 'wb')
+    pickle.dump(obj, pickleFile)
+    pickleFile.close()
+
+def loadData(filename):
+    pickleFile = open(filename, 'rb')
+    obj = pickle.load(pickleFile)
+    pickleFile.close()
+    return obj
+
+port = int(input("Enter port:"))
+file_name = 'port_' + str(port) + '.dat'
+file = 'response_from_iot.dat'
+obj = loadData(file_name)
+
+for value in obj:
+    print(value)
+    print()
